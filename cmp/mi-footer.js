e29a1c557ca97a143e16cce3708d72591466e66b
webdiegooviedo <84927667+webdiegooviedo@users.noreply.github.com>
2021-05-27 14:43:30 -0500

    Update mi-footer.js

--- cmp/mi-footer.js
+++ cmp/mi-footer.js
@@ -1,1 +1,7 @@
-
+class MiFooter extends HTMLElement{
+  connectedCallback(){
+    this.innerHTML = /* HTML */
+      'Copyrigth &copy; 2021 Oviedo Pimentel Diego Emiliano';
+  }
+}
+customElements.define("mi-footer", MiFooter);
