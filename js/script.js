f1761c16394ef3d96eb6d0be9a754af84e34311b
webdiegooviedo <84927667+webdiegooviedo@users.noreply.github.com>
2021-06-22 15:14:00 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -18,3 +18,24 @@ salidaMateria.textContent =txtMateria.value;
 salidaFecha.textContent = txtFecha.value;
 salidaBoleta.textContent = txtBoleta.value;
 }
+function registro() {
+    var email = document.getElementById("emailNuevoUsuario").value
+    var password= document.getElementById("contraseniaNuevoUsuario").value
+      if (email == "" || email == null || password == "" || password == null) {
+        alert("Llena los campos por favor")
+      } else {
+        console.log("Entro a funcion registrarse")
+        console.log("El correo que se va a registrar es:" + email)
+        console.log("El password que se va a registrar es:" + password)
+        firebase.auth().createUserWithEmailAndPassword(email, password).then((user) => {
+          console.log(user)
+          alert("Tu cuenta se creo correctamente")
+          window.location.href = "index.html"
+        }).catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          alert(errorMessage)
+          // ..
+        });
+      }
+    }
